edf735506c1914438d2e97e065290cf5d210825c
Milos Belic <milos98.belic@gmail.com>
2021-07-29 15:25:27 +0200

    Added data layer & basket increment

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css"
 import {Link} from "react-router-dom";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}] = useStateValue();
+
     return (
         <nav className="header">
             <Link to="/">
@@ -43,7 +46,7 @@ function Header() {
                 <Link to="/checkout" className="header__link">
                     <div class="header__optionBasket">
                         <ShoppingBasketIcon/>
-                        <span className="header__optionLineTwo header__basketCount">0</span>
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
                     </div>
                 </Link>
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, image, price, rating}) {
+    const [{}, dispatch] = useStateValue();
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            },
+        })
+    };
     return (
         <div className="product">
             <div class="product__info">
@@ -21,7 +35,7 @@ function Product({id, title, image, price, rating}) {
                 </div>
             </div>
             <img src={image} alt="" />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
